2a084884f7e57cba63d8e34b3a74f99cdf484f1c
j <ox19c@foxmail.com>
2019-08-04 10:19:58 +0800

    prepare build for mac

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/google/flatbuffers v1.11.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/stretchr/testify v1.3.0
+	google.golang.org/appengine v1.6.1 // indirect
 	soloos/common v0.0.0
 	soloos/sdbone v0.0.0
 	soloos/soloboat v0.0.0
---
+++ memstg/posixfs_inode_attr_darwin.go
@@ -0,0 +1,34 @@
+package memstg
+
+import (
+	"soloos/common/fsapitypes"
+	"soloos/common/sdfsapitypes"
+	"soloos/sdfs/sdfstypes"
+)
+
+func (p *PosixFS) SetFsAttrByFsINode(fsAttr *fsapitypes.Attr, pFsINodeMeta *sdfsapitypes.FsINodeMeta) {
+	fsAttr.Ino = pFsINodeMeta.Ino
+
+	if pFsINodeMeta.NetINodeID != sdfsapitypes.ZeroNetINodeID ||
+		pFsINodeMeta.Type == sdfstypes.FSINODE_TYPE_HARD_LINK {
+		var uFsINode, err = p.FsINodeDriver.GetFsINodeByIDThroughHardLink(pFsINodeMeta.Ino)
+		defer p.FsINodeDriver.ReleaseFsINode(uFsINode)
+		var pFsINode = uFsINode.Ptr()
+		if err == nil && pFsINode.UNetINode != 0 {
+			fsAttr.Size = pFsINode.UNetINode.Ptr().Size
+			fsAttr.Blocks = pFsINode.UNetINode.Ptr().GetBlocks()
+		}
+	}
+
+	fsAttr.Atime = pFsINodeMeta.Atime
+	fsAttr.Ctime = pFsINodeMeta.Ctime
+	fsAttr.Mtime = pFsINodeMeta.Mtime
+	fsAttr.Atimensec = pFsINodeMeta.Atimensec
+	fsAttr.Ctimensec = pFsINodeMeta.Ctimensec
+	fsAttr.Mtimensec = pFsINodeMeta.Mtimensec
+	fsAttr.Mode = pFsINodeMeta.Mode
+	fsAttr.Nlink = uint32(pFsINodeMeta.Nlink)
+	fsAttr.Uid = pFsINodeMeta.Uid
+	fsAttr.Gid = pFsINodeMeta.Gid
+	fsAttr.Rdev = pFsINodeMeta.Rdev
+}
